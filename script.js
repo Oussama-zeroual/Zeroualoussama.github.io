5df9af0b2eee70f7cb7b28a3508e971c491c2711
samzer05 <oussamazeroual1000@gmail.com>
2026-03-24 14:58:53 +0100

    edited

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 document.querySelectorAll('.sidebar a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href'))
             .scrollIntoView({ behavior: 'smooth' });
     });
+=======
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+>>>>>>> ecdb63b (edited)
 });
